7eb8da743c27ae179abe251b827e12ebb0ea0b48
khg <winquake3@hanmail.net>
2022-11-14 03:52:05 +0900

    초기 커밋.

---
+++ cpp_ex/api_ex_01.cpp
@@ -0,0 +1,8 @@
+#include <Windows.h>
+#include <tchar.h>
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+
+}
