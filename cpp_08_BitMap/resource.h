a97262d6164e37cc396c075fa43d4b4d1645f9aa
khg <winquake3@hanmail.net>
2022-11-30 01:28:50 +0900

    단축키 구현

---
+++ cpp_08_BitMap/resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// cpp_08_BitMap.rc에서 사용되고 있습니다.
+//
+#define IDR_MENU1                       101
+#define IDR_ACCELERATOR5_1              102
+#define ID_FILENEW                      40008
+#define ID_FILEOPEN                     40009
+#define ID_FILESAVE                     40010
+#define ID_EXIT                         40011
+#define ID_EDITUNDO                     40012
+#define ID_EDITCOPY                     40013
+#define ID_EDITPASTE                    40014
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40024
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
