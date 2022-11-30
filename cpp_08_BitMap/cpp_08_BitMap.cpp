6a240cae9c78806808a6ee5afad1a7ef4a020b22
khg <winquake3@hanmail.net>
2022-12-01 06:06:42 +0900

    비트맵, 더블 버퍼링 구현

--- cpp_08_BitMap/cpp_08_BitMap.cpp
+++ cpp_08_BitMap/cpp_08_BitMap.cpp
@@ -3,6 +3,7 @@
 #include <math.h>
 #include  "resource.h"
 #include <stdio.h>
+#include <iostream>
 #define BSIZE 40
 double LengthPts(int x1, int y1, int x2, int y2)
 {
@@ -17,6 +18,9 @@ BOOL InCircle(int x, int y, int mx, int my)
 
 //콜백 함수 선언
 LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+//비트맵
+HINSTANCE hInst;
+
 
 //메인 부분 
 //hInstance : 운영체제의 커널이 응용 프로그램에 부여한 ID로 메모리에서의 위치
@@ -28,6 +32,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 	MSG msg;
 	WNDCLASS WndClass;
 	HACCEL hAcc;
+
+	hInst = hInstance;
 	hAcc = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR5_1));
 	WndClass.style = CS_HREDRAW | CS_VREDRAW; //윈도우의 크기를 변경하면 다시 그리는 형태의 윈도우를 뜻한다.
 	WndClass.lpfnWndProc = WndProc; //메세지 처리를 위한 함수
@@ -98,14 +104,33 @@ void OutFromFile(TCHAR filename[], HWND hwnd)
 	ReleaseDC(hwnd, hdc);
 }
 
+void TextPrint(HDC hdc, int x, int y, TCHAR text[])
+{
+	int i, j;
+	SetTextColor(hdc, RGB(255, 255, 255));
+	for ( i = -1; i <= 1;  i++)
+	{
+		for ( j = -1; j < 1; j++)
+		{
+			TextOut(hdc, x+i, y+j, text, _tcslen(text));
+		}
+		SetTextColor(hdc, RGB(0, 0, 0));
+		TextOut(hdc, x, y, text, _tcslen(text));
+	}
+}
+
 //메세지 처리 부분, 콜백 함수 명시
 LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 {
 	int answer;
 	OPENFILENAME OFN;
 	OPENFILENAME SFN;
-	HDC hdc;
+	HDC hdc, mem1dc, mem2dc;
 	PAINTSTRUCT ps;
+	static HBITMAP hBitmap, hBit1, hBit2, oldBit1, oldBit2;
+	static RECT rectView;
+	static int yPos;
+	TCHAR word[] = _T("장원영~~ >.<");
 
 	TCHAR str[100], lpstrFile[100] = _T(""); //파일 경로 저장을 위한 배열
 	TCHAR filter[] = _T("전체 파일(*.*) \0*.*\0Text File\0*.txt; *.doc\0");
@@ -132,14 +157,48 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 	switch (iMsg)  //메시지 번호, 처리할 메시지만 case에 나열
 	{
 	case WM_CREATE: //윈도우 창 시작시 사용
+		hBitmap = (HBITMAP)LoadBitmap(hInst, MAKEINTRESOURCE(IDB_BITMAP1));
+		yPos = -30;
+		GetClientRect(hwnd, &rectView);
+		SetTimer(hwnd, 1, 70, NULL);
+		hBit2 = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_BITMAP1));
+		break;
 
+	case WM_TIMER:
+		yPos += 1;
+		if (yPos > rectView.bottom)
+		{
+			yPos = -30;
+		}
+		InvalidateRgn(hwnd, NULL, TRUE);
 		break;
 
 	case WM_PAINT:
-		//복사, 붙이기 활성, 비활성 선택
-		EnableMenuItem(hSubMenu, ID_EDITCOPY, Select ? MF_ENABLED : MF_GRAYED);
-		EnableMenuItem(hSubMenu, ID_EDITPASTE, Copy ? MF_ENABLED : MF_GRAYED);
+		GetClientRect(hwnd, &rectView);
 		hdc = BeginPaint(hwnd, &ps);
+
+		//mem2dc -> mem1dc -> hdc
+		mem1dc = CreateCompatibleDC(hdc);
+		mem2dc = CreateCompatibleDC(mem1dc);
+
+		// 버퍼(hBit1)가 다 비워지면 hdc와 호환 되도록 만든다.
+		if (hBit1 == NULL)
+			hBit1 = CreateCompatibleBitmap(hdc, 1200, 900);
+
+		oldBit1 = (HBITMAP)SelectObject(mem1dc, hBit1);
+		oldBit2 = (HBITMAP)SelectObject(mem2dc, hBit2);
+
+		BitBlt(mem1dc, 0, 0, 1200, 900, mem2dc, 0, 0, SRCCOPY);
+		SetBkMode(mem1dc, TRANSPARENT);
+		TextPrint(mem1dc, 200, yPos, word);
+		BitBlt(hdc, 0, 0, 1200, 900, mem1dc, 0, 0, SRCCOPY);
+
+		SelectObject(mem1dc, oldBit1);
+		SelectObject(mem2dc, oldBit2);
+		DeleteDC(mem2dc);
+		DeleteDC(mem1dc);
+		TextOut(hdc, 200, yPos, word, _tcslen(word));
+
 
 		EndPaint(hwnd, &ps);
 		break;
@@ -199,6 +258,9 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		break;
 
 	case WM_DESTROY: //윈도우 창 종료시 사용
+		if (hBit1) DeleteObject(hBit1);
+		DeleteObject(hBit2);
+		KillTimer(hwnd, 1);
 		PostQuitMessage(0);  //반복 종료를 위해 0 반환
 		break;
 	}
--- cpp_08_BitMap/resource.h
+++ cpp_08_BitMap/resource.h
@@ -4,6 +4,8 @@
 //
 #define IDR_MENU1                       101
 #define IDR_ACCELERATOR5_1              102
+#define IDR_JPG1                        104
+#define IDB_BITMAP1                     105
 #define ID_FILENEW                      40008
 #define ID_FILEOPEN                     40009
 #define ID_FILESAVE                     40010
@@ -16,7 +18,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40024
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
