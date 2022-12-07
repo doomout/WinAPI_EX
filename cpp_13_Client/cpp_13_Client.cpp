0ccea035ad654c4fa7a867a5ae3fad2a5bc99ff5
khg <winquake3@hanmail.net>
2022-12-07 11:40:54 +0900

    소켓 통신 구현

--- cpp_13_Client/cpp_13_Client.cpp
+++ cpp_13_Client/cpp_13_Client.cpp
@@ -1,6 +1,9 @@
 #include <windows.h>
 #include <TCHAR.H>
 
+#pragma comment(lib, "ws2_32.lib")
+
+
 //프로젝트 -> 속성 -> 링커 -> 명령줄에 ws2_32.lib를 추가 해야 한다.
 //소켓 프로그래밍은 WinMain 이 아닌 main으로 해야 에러가 나지 않는다.
 int WINAPI main(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
@@ -13,8 +16,12 @@ int WINAPI main(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine,
 	addr.sin_family = AF_INET;
 	addr.sin_port = 2000; //임의 포트 번호
 	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	connect(s, (LPSOCKADDR)&addr, sizeof(addr));
+	if (connect(s, (LPSOCKADDR)&addr, sizeof(addr)) == -1)
+	{
+		return 0;
+	}
+	send(s, "안녕하세요 Server!", 19, 0); //서버 접속되면 출력되는 문자열
 	closesocket(s);
 	WSACleanup();
-	return 0;
+	return 1;
 }
